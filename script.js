988ca24928ad11121c773051f3cce936c3ee2732
vanessxa <silva.sampaio.vanessa@escola.pr.gov.br>
2025-10-03 12:02:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 function mostraPalavrasChave() {
     const texto = document.querySelector('#entrada-de-texto').value;
     const campoResultado = document.querySelector('#resultado-palavrachave');
-    const palavras = texto.split(" ");
+    const palavrasChave = processaTexto(texto);
 
-    campoResultado.textContent = palavras.join(', ');
+    ampoResultado.textContent = palavrasChave.join(', ');
 }
