166c653f0e049a1836a769081ce424bc8dde3a65
vanessxa <silva.sampaio.vanessa@escola.pr.gov.br>
2025-10-03 12:10:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,5 +7,32 @@ function mostraPalavrasChave() {
     const campoResultado = document.querySelector('#resultado-palavrachave');
     const palavrasChave = processaTexto(texto);
 
-    ampoResultado.textContent = palavrasChave.join(', ');
+    campoResultado.textContent = palavrasChave.join(', ');
+}
+
+
+function processaTexto(texto) {
+    let palavras = texto.split(/\P{L}+/u);
+    const frequencias = contaFrequencias(palavras);
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+
+    function ordenaPalavra(p1, p2) {
+        return frequencias[p2] - frequencias[p1];
+    }
+
+    console.log(ordenadas);
+    return ordenadas.slice(0, 10);
+}
+
+function contaFrequencias(palavras) {
+    let frequencias = {};
+    for (let i of palavras) {
+        frequencias[i] = 0;
+        for (let j of palavras) {
+            if (i == j) {
+                frequencias[i]++;
+            }
+        }
+    }
+    return frequencias;
 }
