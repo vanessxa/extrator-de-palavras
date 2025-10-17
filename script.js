c82d4b8fac3f0c57f94dbb91cd8a0e44e945179d
vanessxa <silva.sampaio.vanessa@escola.pr.gov.br>
2025-10-17 12:11:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { PALAVRAS_RUINS } from "./palavrasRuins.js";
+
 const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 
 botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
@@ -10,29 +12,42 @@ function mostraPalavrasChave() {
     campoResultado.textContent = palavrasChave.join(', ');
 }
 
-
 function processaTexto(texto) {
-    let palavras = texto.split(/\P{L}+/u);
-    const frequencias = contaFrequencias(palavras);
-    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+    // Divide texto em palavras (Unicode), remove vazios
+    let palavras = texto.split(/\P{L}+/u).filter(Boolean);
 
-    function ordenaPalavra(p1, p2) {
-        return frequencias[p2] - frequencias[p1];
+    // Converte para minúsculas
+    for (let i in palavras) {
+        palavras[i] = palavras[i].toLowerCase();
     }
 
-    console.log(ordenadas);
+    // Remove palavras ruins e muito curtas
+    palavras = tiraPalavrasRuins(palavras);
+
+    // Conta frequência das palavras
+    const frequencias = contaFrequencias(palavras);
+
+    // Ordena pela frequência descrescente
+    let ordenadas = Object.keys(frequencias).sort((p1, p2) => frequencias[p2] - frequencias[p1]);
+
+    // Retorna as 10 mais frequentes
     return ordenadas.slice(0, 10);
 }
 
 function contaFrequencias(palavras) {
     let frequencias = {};
-    for (let i of palavras) {
-        frequencias[i] = 0;
-        for (let j of palavras) {
-            if (i == j) {
-                frequencias[i]++;
-            }
-        }
+    for (let palavra of palavras) {
+        frequencias[palavra] = (frequencias[palavra] || 0) + 1;
     }
     return frequencias;
+}
+
+function tiraPalavrasRuins(palavras) {
+    const palavrasBoas = [];
+    for (let palavra of palavras) {
+        if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2) {
+            palavrasBoas.push(palavra);
+        }
+    }
+    return palavrasBoas;
 }
